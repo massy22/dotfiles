39e969d2c0c5c562bfe5d5b7ba0a1f4c5c485789
Kazuki Mashiko <kazuki.mashiko@drecom.co.jp>
2015-05-27 11:17:30 +0900

    sed の代わりに gsed 使ってエラー回避してみたり tmuxx 使うのやめてみたり pbcopy へ文字列を渡すステップを減らしてみたりした

--- .zsh/percol-sources/select-history.zsh
+++ .zsh/percol-sources/select-history.zsh
@@ -1,7 +1,8 @@
 function percol-select-history() {
   local tac_cmd
   which gtac &> /dev/null && tac_cmd=gtac || tac_cmd=tac
-  BUFFER=$($tac_cmd ~/.zsh_history | sed 's/^: [0-9]*:[0-9]*;//' \
+  which gsed &> /dev/null && sed_cmd=gsed || sed_cmd=sed
+  BUFFER=$($tac_cmd ~/.zsh_history | $sed_cmd 's/^: [0-9]*:[0-9]*;//' \
     | percol --match-method regex --query "$LBUFFER")
   CURSOR=$#BUFFER         # move cursor
   zle -R -c               # refresh
