15ab2434f0cd923341a2363ab51785a00b057526
Kazuki Mashiko <kazuki.mashiko@drecom.co.jp>
2018-08-21 12:11:37 +0900

    pecoを導入してzshのhistoryに使うようにした
    [pecoを導入してzshのhistoryに使うようにした](http://blog.kenjiskywalker.org/blog/2014/06/12/peco/)

--- .zsh/peco.zsh
+++ .zsh/peco.zsh
@@ -1,12 +1,12 @@
-function percol-select-history() {
+function peco-select-history() {
   local tac_cmd
   which gtac &> /dev/null && tac_cmd=gtac || tac_cmd=tac
   which gsed &> /dev/null && sed_cmd=gsed || sed_cmd=sed
   BUFFER=$($tac_cmd ~/.zsh_history | $sed_cmd 's/^: [0-9]*:[0-9]*;//' \
-    | percol --match-method regex --query "$LBUFFER")
+    | peco --query "$LBUFFER")
   CURSOR=$#BUFFER         # move cursor
   zle -R -c               # refresh
 }
-zle -N percol-select-history
+zle -N peco-select-history
 
-bindkey '^r' percol-select-history
+bindkey '^r' peco-select-history
